721b9900b42cba62de39a7fd570eb53ba3197519
Thi Nguyen <tttn.tn13@gmail.com>
2022-02-02 15:35:30 -0800

    commit

---
+++ Tree/Binary-Tree/traversal.py
@@ -0,0 +1,19 @@
+# Inorder, Preorder, Postorder traversal methods
+def traversal(root):
+    def inorder(root):  # left node right
+        if root:
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+
+    def preorder(root):  # node left right
+        if root:
+            res.append(root.val)
+            preorder(root.left)
+            preorder(root.right)
+
+    def postorder(root):  # left right node
+        if root:
+            postorder(root.left)
+            postorder(root.right)
+            res.append(root.val)
